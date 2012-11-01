ba626515d032642029b1ebca0cbe98188cf67b04
Partizanes <partizanes@bigmir.net>
2012-11-02 00:46:42 +0400

    [68]Изменение запроса в базу

--- PriceChecker.cpp
+++ PriceChecker.cpp
@@ -247,7 +247,7 @@ Void Form1::query(String^ bar)
 			 "FROM trm_in_var C \n"
 			 "LEFT JOIN trm_in_items A ON A.id=C.item \n"
 			 "LEFT JOIN trm_in_pricelist_items B ON B.item=c.item \n"
-			 "WHERE C.id='"+bar+"' \n"
+			 "WHERE C.item='"+bar+"' \n"
 			 "AND (b.pricelist_id="+CharToSystemString(buf)+")", server1Conn);
 
 		MySqlDataReader^ reader = cmd->ExecuteReader();
